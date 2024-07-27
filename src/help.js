b9958faa28615e492713dec216db3f6ea42a2bf3
sinhaniket <aniket.sinha@bipolarfactory.com>
2024-07-28 00:38:00 +0530

    added backend

--- src/help.js
+++ src/help.js
@@ -1,4 +1,4 @@
-// const port = "https://corporateyatra-backend-2.onrender.com"
-const port = "http://localhost:8080"
+const port = "https://corporateyatra-backend-2.onrender.com"
+// const port = "http://localhost:8080"
 
 export default port;
