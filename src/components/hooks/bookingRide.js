22b712695f275ba4303ff7505d9ade47149ee430
sinhaniket <aniket.sinha@bipolarfactory.com>
2024-07-28 13:37:16 +0530

    update

--- src/components/hooks/bookingRide.js
+++ src/components/hooks/bookingRide.js
@@ -5,20 +5,22 @@ const handleBookRideRegistration = async (sendDestination, sendSource, vehicleID
 
     console.log('sendDestination', sendDestination, "sendSource", sendSource, "vehicleID", "vehicleID")
     try {
+        const accessToken = Cookies.get("accessToken")
+        // const cookies=req.headers.
         const res = await fetch(`${port}/api/v1/users/booking`, {
             method: "POST",
             headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json",
+                "Authorization": `Bearer ${accessToken}`
             },
-            credentials: "include",
+            // credentials: "include",
             body: JSON.stringify({
                 vehicleID, sendSource, sendDestination
             })
         })
         if (res.ok) {
             const data = await res.json()
-
             console.log(data)
         }
     }
